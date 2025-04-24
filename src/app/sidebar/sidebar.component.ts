899c434c35def74e7a7100e5b7fe84ed87e9cd15
OmarMansour101 <mohamedomar.mansour@esprit.tn>
2025-04-24 22:54:15 +0100

    Sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+// sidebar.component.ts
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  categories: Category[] = [];
+  errorMessage = '';
 
+  constructor(private categoryService: CategoryService) { }
+
+  ngOnInit(): void {
+    this.loadCategories();
+  }
+
+  loadCategories(): void {
+    this.categoryService.getCategories().subscribe({
+      next: (categories) => {
+        this.categories = categories;
+      },
+      error: (err) => {
+        console.error('Error loading categories:', err);
+        this.errorMessage = 'Failed to load categories';
+      }
+    });
+  }
 }
