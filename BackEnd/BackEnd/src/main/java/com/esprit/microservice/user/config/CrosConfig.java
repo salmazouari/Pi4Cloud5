53e509ae54cd13aaeee56028cc40310a266d03f0
oumaima27 <maghraouioumaima0@gmail.com>
2025-05-11 21:27:46 +0100

    Merged updated BackEnd from local copy into Oumaima-Maghraoui branch

---
+++ BackEnd/BackEnd/src/main/java/com/esprit/microservice/user/config/CrosConfig.java
@@ -0,0 +1,25 @@
+package com.esprit.microservice.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CrosConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow all origins, headers, and methods for development
+        config.addAllowedOrigin("http://localhost:4200"); // Your Angular app
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
+
